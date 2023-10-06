3747fb8af1ffa718054b2191f6ab2cd7b48fd068
AndreyPushkinA <57715810+AndreyPushkinA@users.noreply.github.com>
2023-10-06 16:16:09 +0200

    Update init.sh

--- postgres/init.sh
+++ postgres/init.sh
@@ -2,4 +2,5 @@
 set -e
 
 # Insert configuration after the comment "# IPv4 local connections:"
-sed -i '/# IPv4 local connections:/a host all all 0.0.0.0/0 md5' "$PGDATA/pg_hba.conf"
+sed -i '/# IPv4 local connections:/a host    all             all             0.0.0.0/0               md5' "$PGDATA/pg_hba.conf"
+sed -i '/# IPv6 local connections:/a host    all             all             ::/0                    md5' "$PGDATA/pg_hba.conf"
