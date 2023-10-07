cebfc0238ac4dabdf4e12c0cdd5d10494d9ad43e
AndreyPushkinA <57715810+AndreyPushkinA@users.noreply.github.com>
2023-10-07 12:05:37 +0200

    Update init.sh

--- postgres/init.sh
+++ postgres/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
-# Insert configuration after the comment "# IPv4 local connections:"
 sed -i '/# IPv4 local connections:/a host    all             all             0.0.0.0/0               md5' "$PGDATA/pg_hba.conf"
 sed -i '/# IPv6 local connections:/a host    all             all             ::/0                    md5' "$PGDATA/pg_hba.conf"
+
+echo "wal_level = logical" >> $PGDATA/postgresql.conf
